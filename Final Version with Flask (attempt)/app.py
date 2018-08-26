85a284e2ed9390da306cf77567f35175312aef23
tonianderson7329 <33438190+tonianderson7329@users.noreply.github.com>
2018-08-25 23:10:48 -0500

    Add files via upload

---
+++ Final Version with Flask (attempt)/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify, render_template, redirect
+# from flask_mysqldb import MySQL
+import os
+
+import pandas as pd
+import numpy as np
+import json
+from pprint import pprint
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import inspect
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/masterdata.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+#inspector = inspect(db.engine)
+#print(inspector.get_table_names())
+
+# Save references to each table
+# building_permits = Base.classes.building_permits
+# demo_2010 = Base.classes.demo_2010
+# demo_2011 = Base.classes.demo_2011
+# demo_2012 = Base.classes.demo_2012
+# demo_2013 = Base.classes.demo_2013
+# demo_2014 = Base.classes.demo_2014
+# demo_2015 = Base.classes.demo_2015
+# demo_2016 = Base.classes.demo_2016
+
+# MasterData = MasterData()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/powerpoint')
+def powerpoint():
+    return render_template('powerpoint.html')  
+
+@app.route('/data')
+def data():
+    return render_template('data.html')   
+
+@app.route('/audio_stats')
+def audio_stats():
+    return render_template('audio_stats.html')
+
+@app.route('/genre')
+def genre():
+    return render_template('genre.html')  
+
+@app.route('/countries')
+def countries():
+    return render_template('countries.html')   
+
+@app.route('/us_map')
+def us_maps():
+    return render_template('us_map.html')    
+
+@app.route('/countries_chart')  
+def countries_chart():
+    return render_template('countries_chart.html')  
+
+@app.route("/countriesdata")
+def countriesdata():
+    return app.send_static_file('./db/csvData.json')
+
+if __name__ == "__main__":
+    app.run(debug=True,threaded=True)
