fe896ef3cfb4d8533d9e7b52bb2ae016429d7400
tonianderson7329 <33438190+tonianderson7329@users.noreply.github.com>
2018-08-27 08:23:50 -0500

    Update app.py

--- Final Version with Flask (attempt)/app.py
+++ Final Version with Flask (attempt)/app.py
@@ -29,20 +29,7 @@ Base = automap_base()
 
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
-#inspector = inspect(db.engine)
-#print(inspector.get_table_names())
 
-# Save references to each table
-# building_permits = Base.classes.building_permits
-# demo_2010 = Base.classes.demo_2010
-# demo_2011 = Base.classes.demo_2011
-# demo_2012 = Base.classes.demo_2012
-# demo_2013 = Base.classes.demo_2013
-# demo_2014 = Base.classes.demo_2014
-# demo_2015 = Base.classes.demo_2015
-# demo_2016 = Base.classes.demo_2016
-
-# MasterData = MasterData()
 
 @app.route('/')
 def index():
